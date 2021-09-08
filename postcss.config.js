febd8e7221afb1edfc935eae6490aa110ef4455a
Farai <faraimutambara@gmail.com>
2021-09-09 00:10:58 +0200

    added postcss.config for setting env

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const postcssPresetEnv = require('postcss-preset-env');
+
+module.exports = {
+  plugins: [postcssPresetEnv()],
+};
